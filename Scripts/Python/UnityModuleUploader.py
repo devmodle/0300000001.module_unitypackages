62f2cb6f202fe8092f6b48cfd2b32d89ebad56f9
dante <are2341@nate.com>
2020-08-25 01:37:42 +0900

    [이상동] 페이스 북 패키지 추가

---
+++ Scripts/Python/UnityModuleUploader.py
@@ -0,0 +1,134 @@
+import os
+import sys
+
+oProjName = sys.argv[1]
+oCommitMsg = sys.argv[2]
+
+oSubmoduleInfos = [
+	{
+		"Name" : ".UnityModule.Study",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Study.Define",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Study.Utility",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Study.Importer",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Define",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Access",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Factory",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Extension",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Func",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Utility",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Externals",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Ads",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Flurry",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Tenjin",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Facebook",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Firebase",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.UnityService",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Singular",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.GameCenter",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Purchase",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.LocalNoti",
+		"Path" : os.path.join(oProjName, "Packages")
+	},
+
+	{
+		"Name" : ".UnityModule.Common.Importer",
+		"Path" : os.path.join(oProjName, "Packages")
+	}
+]
+
+for oSubmoduleInfo in oSubmoduleInfos:
+	oPath = os.path.join("..", "..", oSubmoduleInfo["Path"], oSubmoduleInfo["Name"])
+	oCurrentPath = os.getcwd()
+
+	if os.path.exists(oPath):
+		os.chdir(oPath)
+
+		os.system(f"git add .")
+		os.system(f"git commit -m '{oCommitMsg}'; git push")
+
+		os.chdir(oCurrentPath)
