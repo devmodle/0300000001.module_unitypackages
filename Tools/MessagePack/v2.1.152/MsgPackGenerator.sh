bf2e7caa586ed16850b792fc963fa6937817c245
dy.lee <dy.lee@ninetap.com>
2020-08-25 17:29:59 +0900

    [이대연] 메세지팩 쉘 스크립트 수정

--- Tools/MessagePack/v2.1.152/MsgPackGenerator.sh
+++ Tools/MessagePack/v2.1.152/MsgPackGenerator.sh
@@ -4,7 +4,7 @@ g_oProjectName=${1}
 g_oPath="$(cd "$(dirname "${0}")" && pwd -P)"
 g_oProjectPathA="${g_oPath}/../../../../../${g_oProjectName}/Assembly-CSharp.csproj"
 g_oProjectPathB="${g_oPath}/../../../../../${g_oProjectName}/Assembly-CSharp-Editor.csproj"
-g_oDefineSymbol="MSG_PACK_ENABLE"
+g_oDefineSymbol="MESSAGE_PACK_ENABLE"
 g_oConditionalSymbol="USE_CUSTOM_PROJ_OPT"
 
 ${g_oPath}/MsgPackCompiler -i ${g_oProjectPathA},${g_oProjectPathB} -o "${g_oPath}/../../../../../${g_oProjectName}/Assets/01.UnityProject/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs" -c ${g_oConditionalSymbol} -ms ${g_oDefineSymbol}
