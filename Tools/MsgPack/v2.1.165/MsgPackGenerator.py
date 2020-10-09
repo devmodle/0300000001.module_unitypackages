885b218efbe918e4f1915e9ec72dda1ec2c12012
dante <are2341@nate.com>
2020-10-10 04:25:30 +0900

    [이상동] MsgPack 스크립트 수정

--- Tools/MsgPack/v2.1.165/MsgPackGenerator.py
+++ Tools/MsgPack/v2.1.165/MsgPackGenerator.py
@@ -7,7 +7,5 @@ oProjPathA = f"../../../../../'{oProjName}'/Assembly-CSharp.csproj"
 oProjPathB = f"../../../../../'{oProjName}'/Assembly-CSharp-Editor.csproj"
 oOutputPath = f"../../../../../'{oProjName}'/Assets/01.UnityProject/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
 
-oDefineSymbol = "MSG_PACK_ENABLE"
 oConditionalSymbol = "USE_CUSTOM_PROJ_OPT"
-
-os.system(f"./MsgPackCompiler -i {oProjPathA},{oProjPathB} -o {oOutputPath} -c {oConditionalSymbol} -ms {oDefineSymbol}")
+os.system(f"./MsgPackCompiler -i {oProjPathA},{oProjPathB} -o {oOutputPath} -c {oConditionalSymbol}")
