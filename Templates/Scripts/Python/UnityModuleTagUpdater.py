83371d76df58328e25d713d986e034550475b80c
dante <are2341@nate.com>
2020-10-19 14:55:04 +0900

    [이상동] 태그 업로드 스크립트 수정

--- Templates/Scripts/Python/UnityModuleTagUpdater.py
+++ Templates/Scripts/Python/UnityModuleTagUpdater.py
@@ -119,6 +119,16 @@ oSubmoduleInfos = [
 	{
 		"Name": ".UnityModule.Common.Importer",
 		"Path": f"{oProjName}/Packages"
+	},
+
+	{
+		"Name": "PluginProjects",
+		"Path": oProjName
+	},
+
+	{
+		"Name": "UnityPackages",
+		"Path": oProjName
 	}
 ]
 
