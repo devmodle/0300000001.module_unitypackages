94d3a6481bef4391c2c368a63cb0a7ef5a957c2c
sd.lee <sd.lee@ninetap.com>
2020-11-17 22:51:57 +0900

    [이상동] MsgPack 관련 스크립트 수정

--- Tools/MsgPack/v2.1.194/MsgPackGenerator.py
+++ Tools/MsgPack/v2.1.194/MsgPackGenerator.py
@@ -5,7 +5,7 @@ oProjName = sys.argv[1]
 
 oProjPathA = f"../../../../../'{oProjName}'/Assembly-CSharp.csproj"
 oProjPathB = f"../../../../../'{oProjName}'/Assembly-CSharp-Editor.csproj"
-oOutputPath = f"../../../../../'{oProjName}'/Assets/01.UnityProject/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
+oOutputPath = f"../../../../../'{oProjName}'/Assets/01.UnityProject/Scripts/Runtime/00.AutoCreate/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
 
 oConditionalSymbol = "USE_CUSTOM_PROJ_OPTS"
 os.system(f"./MsgPackCompiler -i {oProjPathA},{oProjPathB} -o {oOutputPath} -c {oConditionalSymbol}")
