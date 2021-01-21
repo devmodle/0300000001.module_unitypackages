be0cfd07409443899e1e76a3ac700ba8f11dbf97
sd.lee <sd.lee@ninetap.com>
2021-01-21 15:44:02 +0900

    [이상동] MsgPack 스크립트 수정

--- Tools/MsgPack/v2.1.194/Mac/MsgPackGenerator.py
+++ Tools/MsgPack/v2.1.194/Mac/MsgPackGenerator.py
@@ -3,8 +3,8 @@ import sys
 
 oProjName = sys.argv[1]
 
-oProjPath = f"../../../../../../'{oProjName}'"
-oOutputPath = f"../../../../../../'{oProjName}'/Assets/01.UnityProject/Scripts/Runtime/00.AutoCreate/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
+oProjPath = f"../../../../../../\"{oProjName}\""
+oOutputPath = f"../../../../../../\"{oProjName}\"/Assets/01.UnityProject/Scripts/Runtime/00.AutoCreate/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
 
 oConditionalSymbol = "MSG_PACK_ENABLE,USE_CUSTOM_PROJ_OPTS"
-os.system(f"./MsgPackCompiler -i '{oProjPath}' -o '{oOutputPath}' -c '{oConditionalSymbol}' -ms 'MSG_PACK_ENABLE'")
+os.system(f"./MsgPackCompiler -i \"{oProjPath}\" -o \"{oOutputPath}\" -c \"{oConditionalSymbol}\" -ms \"MSG_PACK_ENABLE\"")
