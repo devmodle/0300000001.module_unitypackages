3f558a3cce9785f8f102c2fde12ff93075f99689
dante <are2341@nate.com>
2021-06-06 13:04:09 +0900

    [이상동] 서브 모듈 갱신

--- Tools/MsgPack/v2.2.85/Windows/MsgPackGenerator.py
+++ Tools/MsgPack/v2.2.85/Windows/MsgPackGenerator.py
@@ -6,5 +6,5 @@ oProjName = sys.argv[1]
 oProjPath = f"../../../../../../\"{oProjName}\""
 oOutputPath = f"../../../../../../\"{oProjName}\"/Assets/01.UnityProject/00.AutoCreate/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
 
-oConditionalSymbol = "MSG_PACK_ENABLE,USE_CUSTOM_PROJ_OPTS"
+oConditionalSymbol = "MSG_PACK_ENABLE,USE_AUTO_CREATE"
 os.system(f"dotnet mpc -i \"{oProjPath}\" -o \"{oOutputPath}\" -c \"{oConditionalSymbol}\" -ms \"MSG_PACK_ENABLE\"")
