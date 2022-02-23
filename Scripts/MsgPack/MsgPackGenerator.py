930754d8ffc854344393d8641e50390cc4efb06f
dante <are2341@nate.com>
2022-02-23 23:56:08 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/MsgPack/MsgPackGenerator.py
+++ Scripts/MsgPack/MsgPackGenerator.py
@@ -4,5 +4,5 @@ import sys
 oProjPath = f"../../../../\"{sys.argv[1]}\""
 oOutputPath = f"../../../../\"{sys.argv[1]}\"/Assets/01.SubAutoCreate/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
 
-oConditionalSymbols = f"MSG_PACK_ENABLE,{sys.argv[2]}"
+oConditionalSymbols = f"MSG_PACK_ENABLE,NEWTON_SOFT_JSON_MODULE_ENABLE,{sys.argv[2]}"
 os.system(f"dotnet mpc -i \"{oProjPath}\" -o \"{oOutputPath}\" -c \"{oConditionalSymbols}\" -ms \"MSG_PACK_ENABLE\"")
