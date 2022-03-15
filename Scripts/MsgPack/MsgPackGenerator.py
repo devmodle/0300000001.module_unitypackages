a3a126da45d0403b7ee92e5ff58bd6c106839e27
sd.lee <sd.lee@ninetap.com>
2022-03-16 04:54:45 +0900

    [이상동] MsgPack 스크립트 갱신

--- Scripts/MsgPack/MsgPackGenerator.py
+++ Scripts/MsgPack/MsgPackGenerator.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 oProjPath = f"../../../../\"{sys.argv[1]}\""
-oOutputPath = f"../../../../\"{sys.argv[1]}\"/Assets/01.SubAutoCreate/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
+oOutputPath = f"../../../../\"{sys.argv[1]}\"/Assets/03.SubUnityProject/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
 oConditionalSymbols = f"MSG_PACK_ENABLE,NEWTON_SOFT_JSON_MODULE_ENABLE,{sys.argv[2]}"
 
 os.system(f"dotnet tool restore")
