ac6aae93dad0584db085397008ab6ad6fc985025
sd.lee <sd.lee@ninetap.com>
2022-03-17 16:31:40 +0900

    [이상동]

--- Templates/Scripts/Python/UnityModuleUploader.py
+++ Templates/Scripts/Python/UnityModuleUploader.py
@@ -5,8 +5,10 @@ oProjName = sys.argv[1]
 oCommitMsg = sys.argv[2]
 oBranchName = sys.argv[3]
 
+oCommitCmd = f"git commit -m \"{oCommitMsg}\""
+
 os.system(f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git add .\"")
-os.system(f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"git commit -m {oCommitMsg}\"")
+os.system(f"python3 UnityModuleCmdExecuter.py \"{oProjName}\" \"{oCommitCmd}\"")
 
 # 브랜치 이름이 유효 할 경우
 if len(oBranchName) >= 1:
