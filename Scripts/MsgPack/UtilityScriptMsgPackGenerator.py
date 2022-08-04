2c13d9f5128f34ca340c1ea6b1111d90a43a1997
dante <are2341@nate.com>
2022-08-04 12:21:41 +0900

    [이상동] 서브 모듈 갱신

---
+++ Scripts/MsgPack/UtilityScriptMsgPackGenerator.py
@@ -0,0 +1,4 @@
+import os
+import sys
+
+os.system(f"python3 ./MsgPackGenerator.py \"{sys.argv[1]}\" \"UTILITY_SCRIPT_TEMPLATES_MODULE_ENABLE\"")
--- Templates/Scripts/C#/KDefine+SubStrTable.cs
+++ Templates/Scripts/C#/KDefine+SubStrTable.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 
-#if EXTRA_SCRIPT_MODULE_ENABLE && RUNTIME_TEMPLATES_MODULE_ENABLE
+#if EXTRA_SCRIPT_MODULE_ENABLE && UTILITY_SCRIPT_TEMPLATES_MODULE_ENABLE
 /** 서브 문자열 테이블 상수 */
 public static partial class KDefine {
 	#region 기본
 	//*** Setup KDefine+SubStrTable.cs By LocalizeGenerator ***//
 	#endregion			// 기본
 }
-#endif			// #if EXTRA_SCRIPT_MODULE_ENABLE && RUNTIME_TEMPLATES_MODULE_ENABLE
+#endif			// #if EXTRA_SCRIPT_MODULE_ENABLE && UTILITY_SCRIPT_TEMPLATES_MODULE_ENABLE
