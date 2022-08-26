f683818ac4b2ddc6a2d5d04a18332f4a8ccd837d
sd.lee <sd.lee@ninetap.com>
2022-08-27 00:12:51 +0900

    [이상동] 테이블 관련 구문 수정

--- Scripts/MsgPack/5.x/Silicon/MsgPackGenerator.py
+++ Scripts/MsgPack/5.x/Silicon/MsgPackGenerator.py
@@ -3,7 +3,7 @@ import sys
 
 oProjPath = f"../../../../../../\"{sys.argv[1]}\""
 oOutputPath = f"../../../../../../\"{sys.argv[1]}\"/Assets/02.SubUnityProject/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
-oConditionalSymbols = f"MSG_PACK_ENABLE,EXTRA_SCRIPT_MODULE_ENABLE,NEWTON_SOFT_JSON_MODULE_ENABLE,{sys.argv[2]}"
+oConditionalSymbols = f"MSG_PACK_ENABLE,EXTRA_SCRIPT_MODULE_ENABLE,{sys.argv[2]}"
 
 os.system(f"/usr/local/share/dotnet/x64/dotnet tool restore")
-os.system(f"/usr/local/share/dotnet/x64/dotnet mpc -i \"{oProjPath}\" -o \"{oOutputPath}\" -c \"{oConditionalSymbols}\" -ms \"MSG_PACK_ENABLE&&EXTRA_SCRIPT_MODULE_ENABLE&&NEWTON_SOFT_JSON_MODULE_ENABLE\"")
+os.system(f"/usr/local/share/dotnet/x64/dotnet mpc -i \"{oProjPath}\" -o \"{oOutputPath}\" -c \"{oConditionalSymbols}\" -ms \"MSG_PACK_ENABLE&&EXTRA_SCRIPT_MODULE_ENABLE\"")
