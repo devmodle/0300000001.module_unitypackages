04fa0a04bdc17fa279d7e23515407e72c7431999
dante <lkstudio.dante@gmail.com>
2022-09-16 03:38:01 +0900

    [이상동]

--- Templates/Scripts/Python/UnityModuleUploader.py
+++ Templates/Scripts/Python/UnityModuleUploader.py
@@ -6,7 +6,7 @@ oCommitMsg = sys.argv[2]
 oBranchName = sys.argv[3]
 
 os.system(f"python UnityModuleCmdExecuter.py \"{oProjName}\" \"git add .\"")
-os.system(f"python UnityModuleCmdExecuter.py \"{oProjName}\" \"git commit -m \'{oCommitMsg}\'\"")
+os.system(f"python UnityModuleCmdExecuter.py \"{oProjName}\" \"git commit -m \"{oCommitMsg}\"\"")
 
 # 브랜치 이름이 유효 할 경우
 if oBranchName:
