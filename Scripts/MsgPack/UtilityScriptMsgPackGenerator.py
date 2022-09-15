1885d0b647acf3869c1df5ac70e346efdae7a206
sd.lee <sd.lee@ninetap.com>
2022-09-16 02:53:21 +0900

    [이상동] Python 스크립트 갱신

--- Scripts/MsgPack/UtilityScriptMsgPackGenerator.py
+++ Scripts/MsgPack/UtilityScriptMsgPackGenerator.py
@@ -1,4 +1,4 @@
 import os
 import sys
 
-os.system(f"python3 ./MsgPackGenerator.py \"{sys.argv[1]}\" \"UTILITY_SCRIPT_TEMPLATES_MODULE_ENABLE\"")
+os.system(f"python ./MsgPackGenerator.py \"{sys.argv[1]}\" \"UTILITY_SCRIPT_TEMPLATES_MODULE_ENABLE\"")
