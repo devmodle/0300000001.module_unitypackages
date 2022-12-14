9397795248eb0c12f25fe7ed2b15fc54a7f61d2e
sd.lee <sd.lee@ninetap.com>
2022-12-14 15:04:23 +0900

    [이상동] 자동 생성 디렉토리 이름 변경 (Ex.00.AutoCreate => 00-AutoCreate)

--- Scripts/MsgPack/Mac/Silicon/MsgPackGenerator.py
+++ Scripts/MsgPack/Mac/Silicon/MsgPackGenerator.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 oProjPath = f"../../../../../../\"{sys.argv[1]}\""
-oOutputPath = f"../../../../../../\"{sys.argv[1]}\"/Assets/02.SubUnityProject/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
+oOutputPath = f"../../../../../../\"{sys.argv[1]}\"/Assets/02-SubUnityProject/Scripts/Runtime/Global/Utility/External/MessagePack/CMsgPackResolver.cs"
 oConditionalSymbols = f"MSG_PACK_ENABLE,EXTRA_SCRIPT_MODULE_ENABLE,{sys.argv[2]}"
 
 os.system(f"/usr/local/share/dotnet/x64/dotnet tool restore")
